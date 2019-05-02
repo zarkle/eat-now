5112496cf2992dc36348e0f9466cc6a8cfbdf9df
Beverly Pham <beverly.pham@gmail.com>
2019-05-02 11:20:52 -0700

    create initial file

---
+++ eatnow.py
@@ -0,0 +1,13 @@
+"""."""
+
+
+def main():
+    """."""
+    print('test')
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('\nThanks for visiting.')
