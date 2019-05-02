93c56b543366de2421d5652d5e90e661b44cfe3e
Beverly Pham <beverly.pham@gmail.com>
2019-05-02 14:39:21 -0700

    returning trucks open today, need to filter for time

--- eatnow.py
+++ eatnow.py
@@ -1,16 +1,21 @@
 """
 A CLI for food trucks.
 """
-import requests, json
+import os, requests, json
+from datetime import datetime as DT
 
-url = 'https://data.sfgov.org/resource/jjew-r69b.json'
+# API_URL = 'https://data.sfgov.org/resource/jjew-r69b.json'
+APP_TOKEN = os.getenv('APPTOKEN')
+API_URL = f'https://data.sfgov.org/resource/jjew-r69b.json?$$app_token={APP_TOKEN}'
+
 
 def main():
     """."""
-    response = requests.get(url)
+    today = DT.today().weekday() + 1
+    response = requests.get(f'{API_URL}&dayorder={today}')
     if response.status_code == 200:
         data = response.json()
-    print(data)
+        # print(data)
 
 
 if __name__ == "__main__":
